660ba115c7a89d8703aeeec36d78894a9e7ea9e1
babutabhavya <babutabhavya@gmail.com>
2018-11-11 17:37:05 +0530

    Add files via upload

---
+++ BoundaryDetectionOCR/SVMClassifier.cs
@@ -0,0 +1,41 @@
+﻿/*using SVM;
+using System.IO;
+
+    public class SVMClassifier
+    {
+        Model model;
+        
+
+        public void trainClassifier()
+        {
+            Problem train = Problem.Read(@"e:\ocr\ProcessedImages\train.txt");
+            Parameter param = new Parameter();
+            param.C = 32;
+            param.Gamma = 8;
+            model = Training.Train(train, param);
+        }
+        public void testClassifier()
+       { 
+            Problem test = Problem.Read(@"e:\ocr\input\feature");
+            Prediction.Predict(test, @"E:\ocr\input\result", model, false);
+        }
+    }*/
+
+using SVM;
+
+public class SVMClassifier
+{
+    public void Classify()
+    {
+        Problem train = Problem.Read(@"e:\ocr\ProcessedImages\train.txt");
+        Problem test = Problem.Read(@"e:\ocr\input\feature.txt");
+
+        Parameter param = new Parameter();
+        double C;
+        double Gamma;
+        param.C = 64;
+        param.Gamma = 16;
+        Model model = Training.Train(train, param);
+        Prediction.Predict(test, @"E:\ocr\results.txt", model, false);
+    }
+}
---
+++ BoundaryDetectionOCR/StringValues.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Classifier
+{
+    class StringValues
+    {
+        public string datasetPath = @"E:\ocr\Dataset\Sample0";
+        public string dataStorePath = @"E:\ocr\ProcessedImages\";
+        public string trainFilePath = @"E:\ocr\ProcessedImages\train.txt";
+        public string browseFilePath= "";
+        public string browseFileBoundaryDetectionPath = @"E:\ocr\input\";
+        public string browseFileFeatureExtractionFile = @"E:\ocr\input\feature.txt";
+        public string finalOutputFile = @"E:\ocr\results.txt";
+   
+        public string GetbrowseFilePath()
+        {
+            return browseFilePath;
+        }
+
+        public void SetbrowseFilePath(string value)
+        {
+            browseFilePath = value;
+        }
+        
+    }
+}
